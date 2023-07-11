1846eb438408189dd9006e6d533fb3730d7b90b5
Natan <nataniel08@protonmail.com>
2023-07-11 09:47:52 -0300

    feat: remove hyphens, dots, and slash from CNPJ

--- src/ElectronicPointControl.Library/CNPJ.cs
+++ src/ElectronicPointControl.Library/CNPJ.cs
@@ -1,27 +1,55 @@
 using System;
+using ElectronicPointControl.Library.Exceptions;
 
 namespace ElectronicPointControl.Library
 {
-    public struct CNPJ
+    public class CNPJ
     {
-        public string Value { get; set; }
+        public string cnpj { get; set; }
+
+        public CNPJ(string cNPJValue)
+        {
+            cnpj = cNPJValue;
+            CleanCNPJ();
+            DoValidations();
+        }
+
+        private void CleanCNPJ()
+        {
+            cnpj = cnpj.Trim().Replace(".", "").Replace("-", "").Replace("/", "");
+        }
 
         public void DoValidations()
         {
-            throw new NotImplementedException();
+            AllDigitsAreEquals();
+        }
+
+        private void AllDigitsAreEquals()
+        {
+            bool allAreEqual = true;
+
+            foreach (char digit in cnpj)
+                if (digit != cnpj[0])
+                {
+                    allAreEqual = false;
+                    break;
+                }
+
+            if (allAreEqual)
+                throw new InvalidCNPJException("CNPJ inválido: todos os dígitos iguais");
         }
 
         public override string ToString()
         {
-            return Value.Substring(0, 2)
+            return cnpj.Substring(0, 2)
                    + "."
-                   + Value.Substring(2, 3)
+                   + cnpj.Substring(2, 3)
                    + "."
-                   + Value.Substring(5, 3)
+                   + cnpj.Substring(5, 3)
                    + "/"
-                   + Value.Substring(8, 4)
+                   + cnpj.Substring(8, 4)
                    + "-"
-                   + Value.Substring(12, 2);
+                   + cnpj.Substring(12, 2);
         }
     }
 }

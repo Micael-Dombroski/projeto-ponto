ecc87e4ed4115eadcaace4921b857fdc6245bc81
Natan <nataniel08@protonmail.com>
2023-07-20 17:36:25 -0300

    feat: ensure GetAll return a List<Point>

--- src/ElectronicPointControl.Library/PointCRUD.cs
+++ src/ElectronicPointControl.Library/PointCRUD.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System;
 using System.IO;
 
 namespace ElectronicPointControl.Library
@@ -36,7 +37,29 @@ namespace ElectronicPointControl.Library
 
         public List<Point> GetAll()
         {
-            throw new System.NotImplementedException();
+            List<Point> points = new();
+            using (Stream file = File.Open(filePath, FileMode.Open))
+            {
+                using (StreamReader reader = new(file))
+                {
+                    var line = reader.ReadLine();
+                    while (line != null)
+                    {
+                        var props = line.Split(";");
+                        var point = new Point(employeeRegistration: props[1], startWorkLoad: Convert.ToDateTime(props[2]))
+                        {
+                            ID = Guid.Parse(props[0]),
+                            StartPause = props[3] != "null" ? Convert.ToDateTime(props[3]) : null,
+                            FinishPause = props[4] != "null" ? Convert.ToDateTime(props[4]) : null,
+                            FinishWorkLoad = props[5] != "null" ? Convert.ToDateTime(props[5]) : null
+
+                        };
+                        points.Add(point);
+                        line = reader.ReadLine();
+                    }
+                }
+            }
+            return points;
         }
     }
 }

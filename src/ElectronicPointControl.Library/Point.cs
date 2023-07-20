684be516bd46e36a64a443825e75982dcdaf1ae1
Natan <nataniel08@protonmail.com>
2023-07-20 17:26:45 -0300

    feat: add ID and EmployeeRegistration fields to Point entity

--- src/ElectronicPointControl.Library/Point.cs
+++ src/ElectronicPointControl.Library/Point.cs
@@ -2,17 +2,37 @@ using System;
 
 namespace ElectronicPointControl.Library
 {
-    public struct Point
+    public class Point
     {
-        public DateTime StartTime { get; set; }
-        public DateTime? EndTime { get; set; }
-
+        public Guid ID { get; set; }
+        public string EmployeeRegistration { get; }
+        public DateTime StartWorkLoad { get; set; }
         public DateTime? StartPause { get; set; }
-        public DateTime? EndPause { get; set; }
+        public DateTime? FinishPause { get; set; }
+        public DateTime? FinishWorkLoad { get; set; }
+
+        public Point(string employeeRegistration, DateTime startWorkLoad)
+        {
+            ID = Guid.NewGuid();
+            EmployeeRegistration = employeeRegistration;
+            StartWorkLoad = startWorkLoad;
+        }
 
         public override string ToString()
         {
-            return $"{StartTime};{(StartPause is DateTime ? StartPause : "null")};{(EndPause is DateTime ? EndPause : "null")};{(EndTime is DateTime ? EndTime : "null")}";
+            return $"{ID};{EmployeeRegistration};{StartWorkLoad};{(StartPause is DateTime ? StartPause : "null")};{(FinishPause is DateTime ? FinishPause : "null")};{(FinishWorkLoad is DateTime ? FinishWorkLoad : "null")}";
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Point point &&
+                   point.ID == ID &&
+                   point.EmployeeRegistration == EmployeeRegistration;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(ID, EmployeeRegistration);
         }
     }
 }
